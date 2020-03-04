bedfb6fdb8971d8c6f1d0e3c4a7252d38a1ada06
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-04 11:51:25 -0700

    Refactor UrlForm test

--- src/components/UrlForm/UrlForm.test.js
+++ src/components/UrlForm/UrlForm.test.js
@@ -2,19 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { UrlForm, mapDispatchToProps } from './UrlForm';
 import { addUrl } from '../../actions';
-import { postUrls } from '../../apiCalls';
 
 describe('UrlForm', () => {
-    let wrapper, url2;
+    let wrapper;
 
     beforeEach(() => {
-        wrapper = shallow(<UrlForm />)
-        url2 = {
-            id2: 2,
-            title2: 'Why Is Wales Like This',
-            long_url2: 'https://en.wikivoyage.org/wiki/Llanfairpwllgwyngyll',
-            short_url2: 'http://localhost:3001/useshorturl2',
-        }
+        wrapper = shallow(<UrlForm/>)
     })
 
     it('should match snapshot', () => {
@@ -54,17 +47,3 @@ describe('UrlForm', () => {
         })
     })
 });
-
-it('should call addUrlToStore on submit and clear inputs', () => {
-    const mockEvent = { preventDefault: jest.fn() }
-    const mockDispatch = jest.fn();
-
-    window.fetch = jest.fn().mockImplementation(() => {
-        return Promise.resolve({
-            ok: true,
-            json: () => Promise.resolve(url2)
-        });
-    });
-
-
-})
