544ae0ba9d6e358c7c22a2510279a4964028866a
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-04 09:32:18 -0700

    Refactor App to functional component

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import UrlContainer from '../../components/UrlContainer/UrlContainer';
+import UrlForm from '../../components/UrlForm/UrlForm';
+
+const App = () => {
+    return (
+      <main className="App">
+        <header>
+          <h1>URL Shortener</h1>
+          <UrlForm />
+        </header>
+
+        <UrlContainer urls={this.props.urls}/>
+      </main>
+  );
+}
+
+export default App;
