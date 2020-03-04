b3fb1c23223c666e9d2540e6c086c42a7e13e755
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-04 09:22:44 -0700

    Create reducer to handle that new action creator

--- src/reducers/urls.js
+++ src/reducers/urls.js
@@ -2,6 +2,8 @@ export const urls = (state = [], action) => {
   switch (action.type) {
     case 'SET_URLS':
       return action.urls;
+    case 'ADD_URL':
+      return [...state, action.url];
     default:
       return state;
   }
