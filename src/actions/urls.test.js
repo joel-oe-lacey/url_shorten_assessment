cd8e79a25913c1733fabffc4dc95e763c0915e91
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-04 10:19:18 -0700

    Restructure URL action test mock data

--- src/actions/urls.test.js
+++ src/actions/urls.test.js
@@ -1,54 +1,36 @@
 import * as actions from './index';
 
 describe('actions', () => {
-    it('should have a type of ADD_URL', () => {
-        const id = 1;
-        const title = 'Ask Jeeves Twitch';
-        const long_url = 'https://www.ask.com/culture/twitch-intro?qo=articleNav'
-        const short_url = 'http://localhost:3001/useshorturl1';
+    let url1, url2;
 
-        const url = {
-            id,
-            title,
-            long_url,
-            short_url
+    beforeEach(() => {
+        url1 = {
+            id1: 1,
+            title1: 'Ask Jeeves Twitch',
+            long_url1: 'https://www.ask.com/culture/twitch-intro?qo=articleNav',
+            short_url1: 'http://localhost:3001/useshorturl1'
         }
+        url2 = {
+            id2: 2,
+            title2: 'Why Is Wales Like This',
+            long_url2: 'https://en.wikivoyage.org/wiki/Llanfairpwllgwyngyll',
+            short_url2: 'http://localhost:3001/useshorturl2',
+        }
+    })
+
+    it('should have a type of ADD_URL', () => {
 
         const expectedAction = {
             type: 'ADD_URL',
-            url
+            url: url1
         }
 
-        const result = actions.addUrl(url);
+        const result = actions.addUrl(url1);
 
         expect(result).toEqual(expectedAction);
     });
 
     it('should have a type of SET_URLS', () => {
-        const id1 = 1;
-        const title1 = 'Ask Jeeves Twitch';
-        const long_url1 = 'https://www.ask.com/culture/twitch-intro?qo=articleNav';
-        const short_url1 = 'http://localhost:3001/useshorturl1';
-
-        const url1 = {
-            id1,
-            title1,
-            long_url1,
-            short_url1
-        }
-
-        const id2 = 2;
-        const title2 = 'Why Is Wales Like This';
-        const long_url2 = 'https://en.wikivoyage.org/wiki/Llanfairpwllgwyngyll';
-        const short_url2 = 'http://localhost:3001/useshorturl2';
-
-        const url2 = {
-            id2,
-            title2,
-            long_url2,
-            short_url2
-        }
-
         const expectedAction = {
             type: 'SET_URLS',
             urls: [url1, url2]
